fbb13d48374510d996e6483054fb5816ed9023bd
aviennx <avienn.98@outlook.com>
2024-03-27 12:00:00 +0530

    Add Codeforces contests: 940D2, 941D2

---
+++ 940D2/a.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define vc vector
+#define pii pair <int, int>
+#define int long long
+#define yesno(x) cout << ((x) ? "YES\n" : "NO\n")
+
+const int N = 2e5 + 5;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    unordered_map<int,int> mp;
+    for(int& i :arr){
+        cin >> i;
+        mp[i]++;
+    }
+    int ans = 0;
+    for(auto [v, c] : mp){
+        ans += c / 3;
+    }
+    cout << ans << '\n';
+
+    
+
+
+
+}
+
+
+int32_t main(){
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout); 
+    int t = 1;
+    cin >> t;
+    for (int test_case = 1; test_case <= t; test_case++) {
+        solve();
+    }
+    return 0;
+}
---
+++ 941D2/e.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define vc vector
+#define pii pair <int, int>
+#define int long long
+#define yesno(x) cout << ((x) ? "YES\n" : "NO\n")
+
+const int N = 2e5 + 5;
+
+void solve(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int rmax = 1, rmin = 1;
+    int curr = 1;
+    int orientation = 1;
+    int prev = s[0];
+    for(int i = 1; i < n; i++){
+        char c = s[i];
+        if(c == prev){
+            orientation *= -1;
+            continue;
+        }
+        prev = c;
+        curr += orientation;
+        rmax = max(rmax, curr);
+        rmin = min(rmin, curr);
+    }
+    cout << rmax - rmin + 1 << '\n';
+   
+
+
+}
+
+
+int32_t main(){
+    fast;
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout); 
+    int t = 1;
+    cin >> t;
+    for (int test_case = 1; test_case <= t; test_case++) {
+        solve();
+    }
+    return 0;
+}
