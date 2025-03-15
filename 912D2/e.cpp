29a8c3d6ac680a063de830a8bb7e109ecf92ffab
aviennx <avienn.98@outlook.com>
2025-03-15 12:00:00 +0530

    Add Codeforces contest 912D2

---
+++ 912D2/e.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define vc vector
+#define pii pair <int, int>
+#define int long long
+
+const int N = 2e5 + 5;
+
+void solve(){
+    int n;
+    cin >> n;
+   
+
+
+}
+
+
+int32_t main(){
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout); 
+    int t = 1;
+    cin >> t;
+    for (int test_case = 1; test_case <= t; test_case++) {
+        solve();
+    }
+    return 0;
+}
+
